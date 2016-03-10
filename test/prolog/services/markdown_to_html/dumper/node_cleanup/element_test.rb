4fd7eb7ebc0ae691dba96a8c577986982db0f160
Jeff Dickey <jdickey@seven-sigma.com>
2016-03-10 23:58:39 +0800

    NodeCleanup whitespace enforcement encapsulated in new class.
    `BlankEnforcer` ensures that no string child of a parent node has
    whitespace characters other than spaces, and that no string has
    consecutive whitespaces.
    
    It's not really any *harder* in Ox than Nokogiri; you just have to Think
    Differently.
    
    We're also starting to get to a point where class specialisation is
    threatening to fall off the right side of the page; when code for a class
    method is indented *nine levels*, It's Beginning to Feel A Lot Like Java
    (sung to the popular Christmas-song melody). :mask:
    
    168 tests, 204 assertions, 0 failures, 0 errors, 0 skips
    Coverage: suspended; see prolog_core Issue 1
    RuboCop: 59 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 492.0; method average 3.5; max 8.7 (Prolog::Services::MarkdownToHtml::Dumper::NodeCleanup::Element::BlankEnforcer::call)
    Reek: 0 warnings

--- test/prolog/services/markdown_to_html/dumper/node_cleanup/element_test.rb
+++ test/prolog/services/markdown_to_html/dumper/node_cleanup/element_test.rb
@@ -1,12 +1,48 @@
 
 require 'test_helper'
 
+require 'ox'
+
 require 'prolog/services/markdown_to_html/dumper/node_cleanup/element'
 
 cls_name = Prolog::Services::MarkdownToHtml::Dumper::NodeCleanup::Element
 
 describe "#{cls_name}" do
   let(:described_class) { cls_name }
+  # From Wikipedia English article on John Stuart Mill's work, *On Liberty*,
+  # licensed under The Creative Commons Attribution-ShareAlike license. Chosen
+  # as it comprises a single paragraph with multiple, though simple, child
+  # elements and can be validly broken into multiple physical lines which Ox
+  # (and our code) should have no trouble dealing with.
+  let(:js_mill_markup) do
+    <<~ENDIT
+    <p><i><b>On Liberty</b></i> is a philosophical work by <a
+    href="/wiki/British_philosophy" title="British philosophy">English
+    philosopher</a> <a href="/wiki/John_Stuart_Mill"
+    title="John Stuart Mill">John Stuart Mill</a>, originally intended as a
+    short essay. The work, published in 1859, applies Mill's ethical system of
+    <a href="/wiki/Utilitarianism" title="Utilitarianism">utilitarianism
+    </a> to society and the state.<sup id="cite_ref-1" class="reference"><a
+    href="#cite_note-1"><span>[</span>1<span>]</span></a></sup><sup
+    id="cite_ref-2" class="reference"><a
+    href="#cite_note-2"><span>[</span>2<span>]</span></a></sup> Mill attempts to
+    establish standards for the relationship between <a href="/wiki/Authority"
+    title="Authority">authority</a> and <a href="/wiki/Liberty"
+    title="Liberty">liberty</a>. He emphasizes the importance of <a
+    href="/wiki/Individual" title="Individual">individuality</a> which he
+    conceived as a prerequisite to the higher pleasures—the
+    <a href="/wiki/Summum_bonum" title="Summum bonum">summum bonum</a> of
+    Utilitarianism. Furthermore, Mill criticised the errors of past attempts to
+    defend individuality where, for example, democratic ideals resulted in the
+    <a href="/wiki/Tyranny_of_the_majority"
+    title="Tyranny of the majority">"tyranny of the majority"</a>. Among the
+    standards established in this work are Mill's three basic liberties of
+    individuals, his three legitimate objections to government intervention, and
+    his two maxims regarding the relationship of the individual to society
+    "which together form the entire doctrine of [Mill's] Essay."</p>
+    ENDIT
+  end
+  let(:js_mill) { Ox.parse js_mill_markup }
 
   # ########################################################################## #
   # #####                        INITIALISATION                          ##### #
@@ -16,4 +52,24 @@ describe "#{cls_name}" do
     error = expect { described_class.new }.must_raise ArgumentError
     expect(error.message).must_equal 'missing keyword: element'
   end
+
+  # ########################################################################## #
+  # #####           SINGLE-SPACE ENFORCEMENT WITHIN STRINGS              ##### #
+  # ########################################################################## #
+
+  describe 'replaces all in-string whitespace sequences with a single space' do
+    let(:obj) { described_class.new element: js_mill }
+    let(:actual) { obj.to_node }
+    let(:unwanted_whitespace) { /[\t\r\n\f\v]/ }
+    # `Ox.dump` *always* adds a trailing newline. Kill it.
+    let(:dump) { Ox.dump(actual, indent: -1)[0..-2] }
+
+    it 'replacing non-space whitespace characters' do
+      expect(dump).wont_match unwanted_whitespace
+    end
+
+    it 'contains no consecutive space characters' do
+      expect(dump).wont_match(/\s{2,}/)
+    end
+  end # describe 'replaces all in-string whitespace ... with a single space'
 end
