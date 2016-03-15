4a48d288709a84a994cc05d497fefe8d6b21f445
Jeff Dickey <jdickey@seven-sigma.com>
2016-03-15 13:31:37 +0800

    `MarkdownToHtml::Dumper` proven to work with deep, sloppy markup.
    Given an Element node to dump (to a string), the produced string will now
    be a single line of text with no excess whitespace which would be ignored
    by a browser. This *should* allow us to reliably select ranges within
    that markup without worrying about "is there more space in the markup
    than the browser is telling us about", among other benefits.
    
    146 tests, 171 assertions, 0 failures, 0 errors, 0 skips
    Coverage: suspended; see prolog_core Issue 1
    RuboCop: 51 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 441.4; method average 3.6; max 8.1 (Prolog::UseCases::SummariseContent::KeywordCounter#list_keywords_by_count)
    Reek: 0 warnings

--- test/prolog/services/markdown_to_html/dumper_test.rb
+++ test/prolog/services/markdown_to_html/dumper_test.rb
@@ -33,5 +33,22 @@ describe 'Prolog::Services::MarkdownToHtml::Dumper' do
         expect(obj.to_s).must_equal expected
       end
     end # describe 'an Element containing child elements, it'
+
+    describe 'an Element whose child elements contain non-clean text strings' do
+      let(:markup) do
+        "<div>\n<p>\nThis  is a  test.\n</p>\n" \
+        "<ol>\n  <li>First\n<em>\n<strong>\nitem  </strong></em>\n</li>\n  " \
+        "<li>Second item</li>\n  <li>  Last item </li>\n  </ol></div>"
+      end
+      let(:clean_markup) do
+        '<div><p>This is a test.</p><ol>' \
+        '<li>First <em><strong>item</strong></em></li>' \
+        '<li>Second item</li><li>Last item</li></ol></div>'
+      end
+
+      it 'returns properly cleaned-up markup' do
+        expect(obj.to_s).must_equal clean_markup
+      end
+    end # describe 'an Element whose child elements ... non-clean text strings'
   end # describe 'when initialised with'
 end
