b5ef4642e840fe1054453df2ba7adcda28a6d7c6
Jeff Dickey <jdickey@seven-sigma.com>
2016-05-01 02:41:15 +0800

    Scaffolding to-be-coded tests for to-be-coded code.
    `SummariseArticlesByAuthor` has an initialiser and a `#call` method that
    each have the API we expect to close out the branch with; they're as yet
    empty.
    
    280 tests, 320 assertions, 0 failures, 0 errors, 16 skips
    Coverage: 2295 / 2299 LOC (99.83%) covered.
    RuboCop: 67 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 644.6; method average 3.6; max 5.7 (Prolog::UseCases::ProposeEditContribution::FormObject#wrap_contribution_with)
    Reek: 0 warnings

--- lib/prolog/use_cases/summarise_articles_by_author.rb
+++ lib/prolog/use_cases/summarise_articles_by_author.rb
@@ -4,6 +4,59 @@ module Prolog
   module UseCases
     # Just what it says on the tin: like SummariseContent for a single Author.
     class SummariseArticlesByAuthor
+      def initialize(authoriser:, repository:)
+        @authoriser = authoriser
+        @repository = repository
+        self
+      end
+
+      # The `#call` method returns a Hash that is somewhat different than the
+      # return value from `SummariseContent#call`. This reflects that the author
+      # in question may have published no articles and that, while users other
+      # than the author will receive a summary of only *published* articles, the
+      # author himself should receive a summary including any saved drafts that
+      # he may have created.
+      #
+      # * `:articles` references an array of all `Article` entities which the
+      #   persistence subsystem (or "port") makes available. It is entirely
+      #   plausible that this list might be empty; if it is, then the
+      #   `:keywords_by_frequency` entry (below) will reference an empty array
+      #   as well;.
+      # * `:keywords_by_frequency` references a Hash whose keys are integers and
+      #   whose values are arrays of strings. The value strings, collectively,
+      #   include all keywords defined in all `:articles` by the specified
+      #   author; each keyword occurs a total number of times (in the author's
+      #   articles) matching the index of the hash entry in which it occurs.
+      #   For example, a keyword "dunsel" that occurs 6 times in total would be
+      #   part of the array referenced by `result[:keywords_by_frequency][6]`,
+      #   where `result` was the return value from `#call`. Again, if the author
+      #   specified in the parameter to `#call` has published no articles (or
+      #   drafted none, in the case where this is being called by the current
+      #   user), this will be an empty hash.
+      #
+      # No entries for `:most_recent_articles` or for
+      # `:most_recently_updated_articles` are supplied here, contrary to the
+      # example set by the initial implementation of `SummariseContent`, as we
+      # have realised that the caller of this method is perfectly capable of
+      # doing its own sorting *if needed*.
+      #
+      # Also note that there is no error reporting; any errors that do not cause
+      # the code to fail outright will return empty arrays for `:articles` and
+      # `:keywords`.
+      def call(author_name:)
+        load_article_list(author_name)
+        build_return_hash
+      end
+
+      private
+
+      attr_reader :authoriser, :repository
+
+      def build_return_hash
+      end
+
+      def load_article_list(_author_name)
+      end
     end # class Prolog::UseCases::SummariseArticlesByAuthor
   end
 end
--- test/prolog/use_cases/summarise_articles_by_author_test.rb
+++ test/prolog/use_cases/summarise_articles_by_author_test.rb
@@ -7,6 +7,76 @@ require 'prolog/use_cases/summarise_articles_by_author'
 describe 'Prolog::UseCases::SummariseArticlesByAuthor' do
   let(:described_class) { Prolog::UseCases::SummariseArticlesByAuthor }
 
-  it 'is a class' do
-  end
+  describe 'has initialisation that' do
+    it 'requires two named parameters: :authoriser and :repository' do
+      error = expect { described_class.new }.must_raise ArgumentError
+      expected = 'missing keywords: authoriser, repository'
+      expect(error.message).must_equal expected
+    end
+  end # describe 'has initialisation that'
+
+  describe 'has a #call method that' do
+    let(:obj) { described_class.new authoriser: auth, repository: repo }
+    let(:auth) { 'authoriser' }
+    let(:repo) { 'repo' }
+
+    it 'requires an :author_name parameter' do
+      error = expect { obj.call }.must_raise ArgumentError
+      expect(error.message).must_equal 'missing keyword: author_name'
+    end
+
+    describe 'when called' do
+      describe 'by the specified author' do
+        describe 'with no articles at all, it correctly reports' do
+          it 'no articles'
+
+          it 'no keywords'
+        end # describe 'with no articles at all, it correctly reports'
+
+        describe 'with published articles only, it correctly reports' do
+          it 'articles'
+
+          it 'keywords associated with all articles'
+        end # describe 'with published articles only, it correctly reports'
+
+        describe 'with draft articles only, it correctly reports' do
+          it 'articles'
+
+          it 'keywords associated with all articles'
+        end # describe 'with draft articles only, it correctly reports'
+
+        describe 'with both published and draft articles' do
+          it 'articles'
+
+          it 'keywords associated with all articles'
+        end # describe 'with both published and draft articles'
+      end # describe 'by the specified author'
+
+      describe 'by a user other than the specified author' do
+        describe 'with no articles at all, it correctly reports' do
+          it 'no articles'
+
+          it 'no keywords'
+        end # describe 'with no articles at all, it correctly reports'
+
+        describe 'with published articles only, it correctly reports' do
+          it 'articles'
+
+          it 'keywords associated with all articles'
+        end # describe 'with published articles only, it correctly reports'
+
+        describe 'with draft articles only, it correctly reports' do
+          it 'no articles'
+
+          it 'no keywords'
+        end # describe 'with draft articles only, it correctly reports'
+
+        describe 'with both published and draft articles' do
+          it 'published articles only'
+
+          it 'keywords associated with all published articles'
+        end # describe 'with both published and draft articles'
+      end # describe 'by a user other than the specified author'
+    end # describe 'when called'
+  end # describe 'has a #call method that'
 end
