d929ad1a11fb15a7461324fe7b30539d388d97ec
Jeff Dickey <jdickey@seven-sigma.com>
2016-05-05 01:39:04 +0800

    Introducing the (shell of the) Result class for the use case.
    302 tests, 356 assertions, 0 failures, 0 errors, 0 skips
    Coverage: 2556 / 2558 LOC (99.92%) covered.
    RuboCop: 70 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 696.2; method average 3.6; max 5.7 (Prolog::UseCases::ProposeEditContribution::FormObject#wrap_contribution_with)
    Reek: 0 warnings

--- lib/prolog/use_cases/respond_to_single_proposal.rb
+++ lib/prolog/use_cases/respond_to_single_proposal.rb
@@ -5,11 +5,21 @@ module Prolog
     # Encapsulates logic whereby an Author of an Article may Respond to a
     # Contribution that has been Proposed against it.
     class RespondToSingleProposal
+      # Returns information about the success or failure of the action.
+      class Result
+      end
+
       def initialize(article_repo:, authoriser:, contribution_repo:)
         @article_repo = article_repo
         @authoriser = authoriser
         @contribution_repo = contribution_repo
         self
+      end
+
+      # Reek flags this as a :reek:UtilityFunction -- for now.
+      def call(proposal:)
+        _ = proposal
+        Result.new
       end
 
       private
--- test/prolog/use_cases/respond_to_single_proposal_test.rb
+++ test/prolog/use_cases/respond_to_single_proposal_test.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'test_helper'
+require 'matchers/raise_with_message'
 require 'matchers/raise_with_message_part'
 
 require 'prolog/use_cases/respond_to_single_proposal'
@@ -25,7 +26,32 @@ describe 'Prolog::UseCases::RespondToSingleProposal' do
     end # describe 'requires parameters for'
 
     it 'succeeds when values for all parameters are specified' do
-      expect { described_class.new params }.must_be_silent
+      expect(described_class.new params).must_be_instance_of described_class
     end
   end # describe 'initialisation'
+
+  describe 'has a #call method that' do
+    let(:obj) { described_class.new init_params }
+    let(:init_params) do
+      { article_repo: article_repo, authoriser: authoriser,
+        contribution_repo: contribution_repo }
+    end
+    let(:article_repo) { Object.new }
+    let(:authoriser) { Object.new }
+    let(:contribution_repo) { Object.new }
+    let(:call_params) { { proposal: proposal } }
+    let(:proposal) { Object.new }
+
+    describe 'when called' do
+      it 'without a :proposal parameter, raises an error' do
+        message = 'missing keyword: proposal'
+        expect { obj.call }.must_raise_with_message message
+      end
+
+      it 'with a valid :proposal parameter, returns a Result instance' do
+        result_class = described_class.const_get(:Result)
+        expect(obj.call call_params).must_be_instance_of result_class
+      end
+    end # describe 'when called'
+  end # describe 'has a #call method that'
 end
