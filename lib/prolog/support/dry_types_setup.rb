3a13783487ecc0894ee761e1dd420835943d449c
Jeff Dickey <jdickey@seven-sigma.com>
2016-05-07 00:15:12 +0800

    Added include file for some Dry::Types setup.
    This defines an integer-range attribute type analogous to our existing
    Virtus attribute class definition. We used it in some experiments with
    `Dry::Types` (see Issue #28, in particular the comment from [7 May 2016
    at 00:35:04 SGT](https://github.com/TheProlog/prolog-use_cases/issues/28#issuecomment-217492419).)
    
    We'll be coming back to this.
    
    296 tests, 350 assertions, 0 failures, 0 errors, 0 skips
    Coverage: 2495 / 2497 LOC (99.92%) covered.
    RuboCop: 71 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 691.8; method average 3.6; max 5.7 (Prolog::UseCases::ProposeEditContribution::FormObject#wrap_contribution_with)
    Reek: 0 warnings

---
+++ lib/prolog/support/dry_types_setup.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+require 'dry-types'
+
+# These need to be before `Dryp::Types.module` is included. Also,
+# obviously, after requiring `dry-types`.
+Dry::Types.register 'range', Dry::Types::Definition[Range].new(Range)
+Dry::Types.register 'strict.range',
+                    Dry::Types['range'].constrained(type: Range)
+
+# `Types` as a top-level namespace module seems to be a `dry-types`
+# convention.
+module Types
+  include Dry::Types.module
+
+  IntegerRange = Range.constructor do |value|
+    if value.is_a?(::Range)
+      value
+    elsif value.to_i == 0
+      -1..-1
+    else
+      o..value.to_i
+    end
+  end
+end
