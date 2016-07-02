6089dfbf0cf27bb82e47601d562671c9695da4dd
Jeff Dickey <jdickey@seven-sigma.com>
2016-07-03 02:04:35 +0800

    Extracted `Responded` entity from `Contribution::Accepted` entity
    This is the new common base for both the `Accepted` and `Rejected`
    contribution entities.
    
    363 tests, 415 assertions, 0 failures, 0 errors, 0 skips
    Coverage: 3343 / 3347 LOC (99.88%) covered.
    RuboCop: 103 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 988.6; method average 3.4; max 5.7 (Prolog::UseCases::RegisterNewMember::FormObject#unique_error_messages)
    Reek: 0 warnings

--- lib/prolog/entities/contribution/accepted.rb
+++ lib/prolog/entities/contribution/accepted.rb
@@ -1,37 +1,14 @@
-
 # frozen_string_literal: true
 
-require 'forwardable'
-require 'uuid'
-
-require 'prolog/support/dry_types_setup'
-
-require 'prolog/entities/article_ident_v'
+require_relative './responded'
 
 module Prolog
   module Entities
     module Contribution
-      # Proposed-Contribution entity;
-      class Accepted < Dry::Types::Value
-        extend Forwardable
-
-        attribute :article_id, Prolog::Entities::ArticleIdentV
-        attribute :identifier, Types::UUID
-        attribute :proposal_id, Types::UUID
-        attribute :responded_at, Types::DateTimeOrNow
-        attribute :response_text, Types::Strict::String.default('')
+      # Accepted-Contribution entity.
+      class Accepted < Responded
         attribute :updated_body, Types::Strict::String.constrained(min_size: 1)
-
-        def_delegators :article_id, :author_name, :title
-
-        def type # Not settable as an attribute...yet.
-          :edit
-        end
-
-        def to_h
-          super.merge(type: type)
-        end
-      end # class Entities::Contribution::Accepted
+      end # class Prolog::Entities::Contribution::Accepted
     end
   end
 end
--- test/prolog/entities/contribution/responded_test.rb
+++ test/prolog/entities/contribution/responded_test.rb
@@ -2,12 +2,12 @@
 
 require 'test_helper'
 
-require 'matchers/raise_with_message_part'
+require 'matchers/raise_with_message'
 
-require 'prolog/entities/contribution/accepted'
+require 'prolog/entities/contribution/responded'
 
-describe 'Prolog::Entities::Contribution::Accepted' do
-  let(:described_class) { Prolog::Entities::Contribution::Accepted }
+describe 'Prolog::Entities::Contribution::Responded' do
+  let(:described_class) { Prolog::Entities::Contribution::Responded }
   let(:artid_class) { Prolog::Entities::ArticleIdentV }
 
   let(:all_params) do
@@ -15,44 +15,31 @@ describe 'Prolog::Entities::Contribution::Accepted' do
                   response_text: response_text }
     required_params.merge optionals
   end
+  let(:required_params) do
+    { article_id: article_id, proposal_id: proposal_id,
+      identifier: nil, responded_at: nil, response_text: nil }
+  end
   let(:article_id) { artid_class.new author_name: author_name, title: title }
   let(:author_name) { 'J Random Author' }
   let(:identifier) { '12345678-1234-5678-9012-123456789012' }
-  let(:obj) { described_class.new required_params }
   let(:proposal_id) { '87654321-9876-5432-1098-987654321098' }
-  let(:required_params) do
-    { article_id: article_id, proposal_id: proposal_id,
-      updated_body: updated_body, identifier: nil, responded_at: nil,
-      response_text: nil }
-  end
   let(:responded_at) { DateTime.parse '2 Jun 2016 01:23:45 SGT' }
   let(:response_text) { 'Roger dodger' }
   let(:title) { 'This is a Title' }
-  let(:updated_body) { 'Update your body with exercise.' }
 
   describe 'initialisation' do
     let(:obj) { described_class.new required_params }
 
-    describe 'succeeds with' do
-      after do
-        expect(described_class.new @params).must_be_instance_of described_class
-      end
+    after do
+      expect(described_class.new @params).must_be_instance_of described_class
+    end
 
-      it 'all required parameters' do
-        @params = required_params
-      end
+    it 'succeeds with all required parameters' do
+      @params = required_params
+    end
 
-      it 'all required and optional parameters' do
-        @params = all_params
-      end
-    end # describe 'succeeds with'
-
-    it 'raises an error when an empty string is supplied as :updated_body' do
-      required_params[:updated_body] = ''
-      expected = 'invalid type for :updated_body'
-      expect do
-        described_class.new required_params
-      end.must_raise_with_message_part expected, Dry::Types::StructError
+    it 'succeeds with all required and optional parameters' do
+      @params = all_params
     end
   end # describe 'initialisation'
 
@@ -61,6 +48,8 @@ describe 'Prolog::Entities::Contribution::Accepted' do
   end
 
   describe 'delegates to the :article_id attribute methods for' do
+    let(:obj) { described_class.new required_params }
+
     it ':author_name' do
       expect(obj.author_name).must_equal author_name
     end
@@ -72,6 +61,7 @@ describe 'Prolog::Entities::Contribution::Accepted' do
 
   describe 'has a #to_h method that' do
     it 'returns a hash including a :type value of :edit' do
+      obj = described_class.new required_params
       expect(obj.to_h[:type]).must_equal :edit
     end
   end # describe 'has a #to_h method that'
