64b22c29706952a38df4fe27ec4c923f9365c7f8
Jeff Dickey <jdickey@seven-sigma.com>
2016-07-09 13:34:59 +0800

    SummariseOwnContribHistory returns Contributions in #call result.
    Also, `dry_types_setup.rb` has new definition for
    `Types::ContributionHash`, which is a first whack at the schema for
    those returned Contributions as a Hash with specific keys, and (generic)
    arrays as values. This eliminates the need to test (likely malicious)
    invalid key usage; it's not allowed by the underlying Dry::Types code.
    
    387 tests, 439 assertions, 0 failures, 0 errors, 0 skips
    Coverage: 3532 / 3536 LOC (99.89%) covered.
    RuboCop: 107 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 1017.3; method average 3.3; max 5.7 (Prolog::UseCases::RegisterNewMember::FormObject#unique_error_messages)
    Reek: 0 warnings

--- lib/prolog/use_cases/summarise_own_contrib_history.rb
+++ lib/prolog/use_cases/summarise_own_contrib_history.rb
@@ -1,16 +1,38 @@
 # frozen_string_literal: true
 
+require 'forwardable'
+
+require 'prolog/support/dry_types_setup'
+
 module Prolog
   module UseCases
     # For a logged-in (authenticated) Member, return a Hash containing lists of
     # each Proposed or Responded Contribution by that Member, with accompanying
     # conventional sugar.
     class SummariseOwnContribHistory
+      # Value object communicating result of use case to caller.
+      class Result < Dry::Types::Value
+        attribute :contributions, Types::ContributionHash
+        attribute :errors, Types::Strict::Array
+
+        def success
+          errors.empty?
+        end
+        alias success? success
+
+        def failure
+          !success
+        end
+        alias failure? failure
+      end # class Prolog::UseCases::SummariseOwnContribHistory::Result
+
       def self.call(authoriser:, contribution_repo:)
         SummariseOwnContribHistory.new(contribution_repo).call authoriser
       end
 
-      def call(_authoriser)
+      def call(authoriser)
+        @authoriser = authoriser
+        Result.new errors: [], contributions: all_contributions
       end
 
       protected
@@ -21,7 +43,34 @@ module Prolog
 
       private
 
-      attr_reader :contribution_repo
+      extend Forwardable
+
+      attr_reader :authoriser, :contribution_repo
+      def_delegators :authoriser, :user_name
+
+      def accepted
+        { accepted: contributions(:accepted) }
+      end
+
+      def all_contributions
+        Hash[*collection.flatten(2)]
+      end
+
+      def collection
+        accepted.zip(proposed, rejected)
+      end
+
+      def contributions(status)
+        contribution_repo.find(author_name: user_name, status: status)
+      end
+
+      def proposed
+        { proposed: contributions(:proposed) }
+      end
+
+      def rejected
+        { rejected: contributions(:rejected) }
+      end
     end # class Prolog::UseCases::SummariseOwnContribHistory
   end
 end
--- test/prolog/use_cases/summarise_own_contrib_history_test.rb
+++ test/prolog/use_cases/summarise_own_contrib_history_test.rb
@@ -4,19 +4,44 @@ require 'test_helper'
 
 require 'matchers/requires_initialize_parameter'
 
+require 'prolog/entities/contribution/proposed'
 require 'prolog/use_cases/summarise_own_contrib_history'
 
 describe 'Prolog::UseCases::SummariseOwnContribHistory' do
   let(:described_class) { Prolog::UseCases::SummariseOwnContribHistory }
+  let(:accepted_contributions) { [] }
   let(:authoriser) do
     Struct.new(:guest?, :user_name).new is_guest, user_name
   end
   let(:call_params) do
     { authoriser: authoriser, contribution_repo: contribution_repo }
   end
-  let(:contribution_repo) { :dummy_contribution_repo }
+  let(:contribution_repo) do
+    Class.new do
+      def initialize(proposed: [], accepted: [], rejected: [])
+        @data = {
+          accepted: Array(accepted),
+          proposed: Array(proposed),
+          rejected: Array(rejected)
+        }
+        self
+      end
+
+      def find(**params)
+        if [:accepted, :proposed, :rejected].include? params[:status]
+          @data[params[:status]]
+        else
+          @data.values.flatten
+        end
+      end
+    end.new proposed: proposed_contributions,
+            accepted: accepted_contributions,
+            rejected: rejected_contributions
+  end
   let(:guest_name) { 'Guest User' }
   let(:is_guest) { user_name == guest_name }
+  let(:proposed_contributions) { [] }
+  let(:rejected_contributions) { [] }
   let(:user_name) { 'M Random Member' }
 
   describe 'has a .call method that' do
@@ -27,8 +52,73 @@ describe 'Prolog::UseCases::SummariseOwnContribHistory' do
       end
     end
 
-    it 'succeeds when valid required parameters are specified' do
-      expect { described_class.call call_params }.must_be_silent
-    end
+    describe 'when the :authoriser indicates that a Member is logged in' do
+      describe 'returns a result object that' do
+        let(:call_result) { described_class.call call_params }
+
+        it 'has an :errors attribute which is empty' do
+          expect(call_result.errors).must_be :empty?
+        end
+
+        it 'has a :success attribute returning true' do
+          expect(call_result.success).must_equal true
+        end
+
+        it 'also returns true from #success?' do
+          expect(call_result).must_be :success?
+        end
+
+        it 'has a :failure attribute returning false' do
+          expect(call_result.failure).must_equal false
+        end
+
+        it 'also returns false from #failure?' do
+          expect(call_result).wont_be :failure?
+        end
+
+        describe 'when the current Member has' do
+          let(:contribs) { call_result.contributions }
+
+          describe 'no history of Contributions' do
+            describe 'reports no Contributions have been' do
+              it 'have been :accepted' do
+                expect(contribs[:accepted]).must_be :empty?
+              end
+
+              it 'have been :proposed' do
+                expect(contribs[:proposed]).must_be :empty?
+              end
+
+              it 'have been :rejected' do
+                expect(contribs[:rejected]).must_be :empty?
+              end
+            end # describe 'reports no Contributions have been'
+          end # describe 'no history of Contributions'
+
+          describe 'Proposed Contributions not yet Responded to' do
+            let(:article_id) do
+              artid_params = { title: 'A Title', author_name: 'N Author' }
+              Prolog::Entities::ArticleIdentV.new artid_params
+            end
+            let(:contrib_params) do
+              { article_id: article_id, identifier: identifier,
+                original_content: 'original content',
+                proposed_content: 'replacement content',
+                proposer: 'Joe Bazooka', justification: nil, proposed_at: nil }
+            end
+            let(:identifier) { UUID.generate }
+            let(:proposed_contributions) do
+              [Prolog::Entities::Contribution::Proposed.new(contrib_params)]
+            end
+            let(:accepted_contributions) { [] }
+            let(:rejected_contributions) { [] }
+
+            it 'returns the correct values as :proposed Contributions' do
+              expect(contribs[:proposed]).must_equal proposed_contributions
+            end
+          end # describe 'Proposed Contributions not yet Responded to'
+        end # describe 'when the current Member has'
+      end # describe 'returns a result object that'
+    end # describe 'when the :authoriser indicates that a Member is logged in'
   end # describe 'has a .call method that'
 end
