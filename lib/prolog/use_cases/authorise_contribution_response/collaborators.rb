28556908a68c0bc7dd3285a69d7a45a265656a8c
Jeff Dickey <jdickey@seven-sigma.com>
2016-08-10 01:02:39 +0800

    Replaced RegisterNewMember code/tests with revision Gem.
    Also updated underlying `dry-types` and related Gems to current releases.
    This required one significant change (beyond removing extracted code and
    tests, of course): Value objects are now deep-frozen; our previous
    convention of having "collaborator" collections as `Dry::Types::Value`
    instances where attributes of object instances *within* that Value were
    modifiable is, properly, no more. Those have been changed to
    `Dry::Types::Struct` instead.
    
    312 tests, 357 assertions, 0 failures, 0 errors, 0 skips
    Coverage: 2902 / 2905 LOC (99.9%) covered.
    RuboCop: 101 files inspected, no offenses detected
    Flay: Total score 0
    Flog: Total 830.1; method average 3.3; max 5.7 (Prolog::UseCases::SummariseContent#build_return_hash)
    Reek: 0 warnings

--- lib/prolog/use_cases/authorise_contribution_response/collaborators.rb
+++ lib/prolog/use_cases/authorise_contribution_response/collaborators.rb
@@ -7,9 +7,9 @@ module Prolog
     # Reports whether a current user is authorised to Respond to a Proposed
     # Contribution and that the Proposal has not yet been Responded to.
     class AuthoriseContributionResponse
-      # Collect all collaborators for Authorise Contribution Response use case
-      # in a value object.
-      class Collaborators < ::Dry::Types::Value
+      # Collect all collaborators for Authorise Contribution Response use case.
+      # Cannot be immutable without breaking test code. FIXME
+      class Collaborators < ::Dry::Types::Struct
         attribute :article_repo, Types::Class
         attribute :authoriser, Types::Class
         attribute :contribution_repo, Types::Class
